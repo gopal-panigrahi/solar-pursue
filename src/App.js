beb56961312f5387345d040fa52c500199a8eeb3
Gopal Panigrahi <gopalpanigrahi99@gmail.com>
2021-05-07 12:24:42 +0530

    Removed Result Page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,13 @@ import './App.css';
 import Header from './components/Header.jsx';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import Homepage from './components/Homepage';
-import Result from './components/Result';
 
 function App() {
   return (
     <>
-      <HashRouter>
-        <Header />
-        <Switch>
-          <Route path="/homepage" component={Homepage} />
-          <Route path="/result" component={Result} />
-        </Switch>
-      </HashRouter>
+      <Header />
+      <Homepage />
+      {/* <Footer /> */}
     </>
   );
 }
