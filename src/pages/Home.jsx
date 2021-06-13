641d106e955ec9bd99e62ed06565c8bc585ebe22
Gopal Panigrahi <gopalpanigrahi99@gmail.com>
2021-06-13 12:24:14 +0530

    Updated the instructions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,12 +15,14 @@ function Home() {
         <Card className="mx-5" border="dark" >
           <Card.Header className="text-center"><Button size="lg" block>Instructions</Button></Card.Header>
           <ListGroup variant="flush">
-            <ListGroup.Item>1.If you are want to use our cloud then select cloud tab</ListGroup.Item>
-            <ListGroup.Item>2.Otherwise,, start the docker using the command ...</ListGroup.Item>
-            <ListGroup.Item>3.Enter all the information of the region.</ListGroup.Item>
-            <ListGroup.Item>4.If you are want to use our cloud then select cloud tab.</ListGroup.Item>
-            <ListGroup.Item>5.Otherwise,, start the docker using the command ..</ListGroup.Item>
-            <ListGroup.Item>6.Enter all the information of the region.</ListGroup.Item>
+            <ListGroup.Item>1. This software is used for generating statistical report from the sky image dataset with name as timestamp.</ListGroup.Item>
+            <ListGroup.Item>2. Start the server by running start_server script</ListGroup.Item>
+            <ListGroup.Item>3. Enter all the information of the region which the dataset belongs to. </ListGroup.Item>
+            <ListGroup.Item>4. If you are want to use our cloud then select cloud tab.</ListGroup.Item>
+            <ListGroup.Item>5. Upload the folder or zip and submit. </ListGroup.Item>
+            <ListGroup.Item>6. Click on the Start Processing button. </ListGroup.Item>
+            <ListGroup.Item>6. After processing is completed, Generate the report. </ListGroup.Item>
+            <ListGroup.Item>7. Add comments to the report and take the print. </ListGroup.Item>
           </ListGroup>
         </Card>
       </CardGroup>
